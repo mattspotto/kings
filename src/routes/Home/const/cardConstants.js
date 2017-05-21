d6bd73356c6dd0cb501afcfcef2214028c62de3e
Dylan Colman <dylancol93@gmail.com>
2017-05-21 16:28:18 +1000

    Adding Decks, tips and rules to the JS jason. Aswell as moved tips into tipsConstant.js

--- src/routes/Home/const/cardConstants.js
+++ src/routes/Home/const/cardConstants.js
@@ -1,66 +1,213 @@
 export let suits = [
-    {
-        symbol: '♥',
-        name: 'heart'
-    }, {
-        symbol: '♦',
-        name: 'diamond'
-    }, {
-        symbol: '♣',
-        name: 'club'
-    }, {
-        symbol: '♠',
-        name: 'spade'
-    }
+  {
+    symbol: '♥',
+    name: 'heart'
+  }, {
+    symbol: '♦',
+    name: 'diamond'
+  }, {
+    symbol: '♣',
+    name: 'club'
+  }, {
+    symbol: '♠',
+    name: 'spade'
+  }
 ];
 
 export let ranks = [
-    {
-        name: 'ace',
-        symbol: 'A'
-    }, {
-        name: 'two',
-        symbol: '2'
-    }, {
-        name: 'three',
-        symbol: '3'
-    }, {
-        name: 'four',
-        symbol: '4'
-    }, {
-        name: 'five',
-        symbol: '5'
-    }, {
-        name: 'six',
-        symbol: '6'
-    }, {
-        name: 'seven',
-        symbol: '7'
-    }, {
-        name: 'eight',
-        symbol: '8'
-    }, {
-        name: 'nine',
-        symbol: '9'
-    }, {
-        name: 'ten',
-        symbol: '10'
-    }, {
-        name: 'jack',
-        symbol: 'J'
-    }, {
-        name: 'queen',
-        symbol: 'Q'
-    }, {
-        name: 'king',
-        symbol: 'K'
-    }
+  {
+    name: 'ace',
+    symbol: 'A'
+  }, {
+    name: 'two',
+    symbol: '2'
+  }, {
+    name: 'three',
+    symbol: '3'
+  }, {
+    name: 'four',
+    symbol: '4'
+  }, {
+    name: 'five',
+    symbol: '5'
+  }, {
+    name: 'six',
+    symbol: '6'
+  }, {
+    name: 'seven',
+    symbol: '7'
+  }, {
+    name: 'eight',
+    symbol: '8'
+  }, {
+    name: 'nine',
+    symbol: '9'
+  }, {
+    name: 'ten',
+    symbol: '10'
+  }, {
+    name: 'jack',
+    symbol: 'J'
+  }, {
+    name: 'queen',
+    symbol: 'Q'
+  }, {
+    name: 'king',
+    symbol: 'K'
+  }
 ];
+
+export let decks = {
+  Classic: {
+    rules: [
+      "waterfall",
+      "forYou",
+      "forMe",
+      "girlsDrink",
+      "neverHaveIEver",
+      "dicks",
+      "heaven",
+      "mate",
+      "rhyme",
+      "categories",
+      "makeARule",
+      "questionMaster",
+      "kingsCup"
+    ],
+    rating: 1
+  },
+  Cavern: {
+    rules: [
+      "aAndB",
+      "forYou",
+      "forMe",
+      "girlsDrink",
+      "neverHaveIEver",
+      "dicks",
+      "gecko",
+      "mate",
+      "mostLikelyToo",
+      "tenSeconds",
+      "makeARule",
+      "questionMaster",
+      "kingsCup"
+    ],
+    rating: 1
+  },
+  ringOfFire: {
+    rules: [
+      "waterfall",
+      "forYou",
+      "forMe",
+      "girlsDrink",
+      "thumbMaster",
+      "dicks",
+      "heaven",
+      "mate",
+      "rhyme",
+      "categories",
+      "makeARule",
+      "questions",
+      "kingsCup"
+    ],
+    rating: 1
+  },
+  daBoiz: {
+    rules: [
+      "waterfall",
+      "forYou",
+      "forMe",
+      "shutUp",
+      "neverHaveIEver",
+      "dicks",
+      "gecko",
+      "mate",
+      "wouldYouRather",
+      "categories",
+      "makeARule",
+      "questionMaster",
+      "kingsCup"
+    ],
+    rating: 1
+  },
+  pro: {
+    rules: [
+      "pissPass",
+      "forYou",
+      "forMe",
+      "mostLikelyToo",
+      "neverHaveIEver",
+      "tellTheTruth",
+      "gecko",
+      "mate",
+      "wouldYouRather",
+      "shoppingList",
+      "makeARule",
+      "questionMaster",
+      "kingsCup"
+    ],
+    rating: 2
+  },
+  hardcore: {
+    rules: [
+      "aAndB",
+      "finish",
+      "itemClothing",
+      "truthDare",
+      "neverHaveIEver",
+      "tellTheTruth",
+      "mostLikelyToo",
+      "mate",
+      "hotSeat",
+      "tenSeconds",
+      "makeARule",
+      "questionMaster",
+      "kingsCup"
+    ],
+    rating: 2
+  },
+  xxx: {
+    rules: [
+      "aAndB",
+      "finish",
+      "itemClothing",
+      "truthDare",
+      "neverHaveIEver",
+      "tellTheTruth",
+      "mostLikelyToo",
+      "truth",
+      "hotSeat",
+      "tenSeconds",
+      "makeARule",
+      "dare",
+      "kingsCup"
+    ],
+    rating: 3
+  },
+  girlsStuff: {
+    rules: [
+      "waterfall",
+      "forYou",
+      "forMe",
+      "girlsDrink",
+      "neverHaveIEver",
+      "mostLikelyToo",
+      "heaven",
+      "tellTheTruth",
+      "wouldILie",
+      "hotSeat",
+      "makeARule",
+      "questions",
+      "kingsCup"
+    ],
+    rating: 2
+  }
+}
 
 export let rules = {
   aAndB: {
     name: 'A & B',
-    tips: 2231,
+    tips: 'aAndB',
     description: 'Player chooses a dare for A & B to participate in'
   },
   forYou: {
@@ -77,7 +224,7 @@ export let rules = {
   },
   neverHaveIEver: {
     name: 'Never Have I Ever',
-    tips: 2235,
+    tips: 'neverHaveIEver',
     description: 'Hold up 3 fingers, say something you haven\'t done and whover has done it must drink and put a finger down'
   },
   dicks: {
@@ -98,90 +245,99 @@ export let rules = {
   },
   categories: {
     name: 'Categories',
-    tips: 2240,
+    tips: 'categories',
     description: 'Pick a category of things, then go round the circle and whoever can\'t think of a related thing must drink'
   },
   makeARule: {
     name: 'Make a Rule',
-    tips: 2241,
+    tips: "makeARule",
     description: 'Player creates a rule that must be adhered to for the rest of the game'
+  },
+  shutUp: {
+    name: 'Silence whore',
+    description: 'First person to makes a noise drinks'
   },
   questionMaster: {
     name: 'Question Master',
-    tips: 2242,
+    tips: 'questionMaster',
     description: 'Whoever answers the player\'s  question must drink'
   },
   kingsCup: {
     name: 'King\'s Cup',
     description: 'Pour a portion of your drink into the Kings Cup'
+  },
+  waterfall: {
+    name: 'Waterfall',
+    description: 'Everyone start drinking. card drawer, can finish at will. rest can only stop when person on their right stops'
+  },
+  mostLikelyToo: {
+    name: 'Most likely too',
+    description: 'Say something someone could or could\'ve done. count down from 3, everyone points at who they think are most likely. most fingers drinks',
+    tips: 'mostLikelyToo'
+  },
+  truth: {
+    name: 'Truth',
+    description: 'Answer truthfully to the first person that asks you any question',
+    tips: 'truth'
+  },
+  dare: {
+    name: 'Dare',
+    description: 'Complete whatever task the first person gives you',
+    tips: 'dare'
+  },
+  truthDare: {
+    name: 'Truth or Dare',
+    description: 'Choose truth or dare. group gets decide on your question/action',
+    tips: 'dare'
+  },
+  shoppingList: {
+    name: 'Shopping list',
+    description: 'Start with, \'I went to the shops and bought; \', add an item, go around clockwise each add to the list'
+  },
+  wouldYouRather: {
+    name: 'Would you rather',
+    description: 'Ask a question with 2 options. count down from 3. hands up for option 1. hands down for option 2. minority drinks.',
+    tips: 'wouldYouRather'
+  },
+  tellTheTruth: {
+    name: 'Tell the Truth',
+    description: 'Ask the group a question(you must answer). Go clockwise. If someone doesn\'t answer they drink. No one answers asign a finish. everyone answers, you finish',
+    tips: 'truth'
+  },
+  wouldILie: {
+    name: 'Would I lie to you?',
+    description: 'Tell a truth or a lie. in 3, 2, 1 everyone either puts hands up for truth or hands down for lie. Incorrect drinks'
+  },
+  tenSeconds: {
+    name: '10 seconds of fame',
+    description: 'Entertain the group for a short period of time (joke, sing, talent...)'
+  },
+  hotSeat: {
+    name: 'Hot Seat',
+    description: 'In a short time, go clockwise. Everyone asks a truth that you answer. If they don\'t have a question, they pass'
+  },
+  pissPass: {
+    name: 'Piss pass',
+    description: 'You may goto the bathroom once with the card. You may trade card for truth, dare or drinks'
+  },
+  finish: {
+    name: 'Finish your drink',
+    description: 'Finish what\'s left in your drink'
+  },
+  itemClothing: {
+    name: 'Item of clothing',
+    description: 'Remove an item of clothing and assign 2 drinks. Pairs equals 1 item'
+  },
+  questions: {
+    name: 'Questions',
+    description: 'Drawer asks a random person a question, then they ask someone. Continues until someone fails to respond with a question quickly or laughs'
+  },
+  gecko: {
+    name: 'Gecko',
+    description: 'Last person on a flat surface (like a gecko) drinks'
+  },
+  thumbMaster: {
+    name: 'Thumb master',
+    description: 'when you\'re ready place your thumb on table, last person drinks'
   }
-};
-
-export let tips = {
-  2231: [
-    {
-      tip: 'A must do a body shot off B',
-      rating: 'A'
-    },
-    {
-      tip: 'Swap shirts',
-      rating: 'A'
-    },
-    {
-      tip: 'A and B must arm wrestle and the winner must finish their drink',
-      rating: 'N'
-    }
-  ],
-  2235: [
-    {
-      tip: '... had sex in public',
-      rating: 'A'
-    },
-    {
-      tip: '... used a sex toy',
-      rating: 'A'
-    },
-    {
-      tip: '... travelled overseas',
-      rating: 'N'
-    },
-    {
-      tip: '... broken a bone',
-      rating: 'N'
-    }
-  ],
-  2240: [
-    {
-      tip: 'Olympic events',
-      rating: 'N'
-    },
-    {
-      tip: 'Alcohol brands',
-      rating: 'N'
-    }
-  ],
-  2241: [
-    {
-      tip: 'Little green man: take an invisible figure off your glass when you drink and place it back when you have finished',
-      rating: 'N'
-    },
-    {
-      tip: 'Gentleman\'s Rules: No swearing, no pointing, no first names',
-      rating: 'N'
-    },
-    {
-      tip: 'Double Tap: When you place your drink down you must tap it twice',
-      rating: 'N'
-    }
-  ],
-  2242: [
-    {
-      tip: 'What does this card do again?',
-      rating: 'N'
-    },
-    {
-      tip: 'What time is it?',
-      rating: 'N'
-    }
-  ]
 };
---
+++ src/routes/Home/const/tipsConstant.js
@@ -0,0 +1,709 @@
+export let tips = {
+  aAndB:[
+    {
+      tip: 'Stare into eachothers eyes for the round. Drink whenever; eye contact is broken or you laugh',
+      rating:  2
+    },
+    {
+      tip: 'Stare into eachothers eyes for the game caress each others face when someone laughs. & you\’re mates',
+      rating:  3
+    },
+    {
+      tip: 'A must do a body shot off B',
+      rating:  2
+    },
+    {
+      tip: 'Swap shirts',
+      rating:  2
+    },
+    {
+      tip: 'Must arm wrestle and the winner must finish their drink',
+      rating:  1
+    },
+    {
+      tip: 'Swapsies: Swap clothes and identity till EOG (active in mini games)',
+      rating:  2
+    },
+    {
+      tip: 'Do a 5 step in-sync handshake on the spot',
+      rating:  1
+    },
+    {
+      tip: 'Attached at the hip for the rest of the game',
+      rating:  2
+    },
+    {
+      tip: 'Lock eyes. In turns have 3 goes at giving each other the zanniest insult. Loser finishes.(decided by group)',
+      rating:  3
+    },
+    {
+      tip: 'Must be attached at the hip for the rest of the game',
+      rating:  2
+    },
+    {
+      tip: 'A has to bite Bs nose',
+      rating:  2
+    },
+    {
+      tip: 'B licks As face while A moans',
+      rating:  3
+    },
+    {
+      tip: 'B make a mystery drink for A',
+      rating:  2
+    },
+    {
+      tip: 'A make a mystery meal for B',
+      rating:  2
+    },
+    {
+      tip: 'A & B go around giving everybody 30sec massage',
+      rating:  1
+    },
+    {
+      tip: 'A do 15 pushups while B yells motivational support',
+      rating:  1
+    },
+    {
+      tip: 'A & B have a 30sec tickle war',
+      rating:  1
+    },
+    {
+      tip: 'A tell a joke & B tell a riddle',
+      rating:  1
+    },
+    {
+      tip: 'A whispers erotic things to B while B skulls',
+      rating:  2
+    },
+    {
+      tip: 'A & B swap names',
+      rating:  1
+    }
+  ],
+  neverHaveIEver:[
+    {
+      tip: '... had sex in public',
+      rating:  2
+    },
+    {
+      tip: '... had sex in a party',
+      rating:  2
+    },
+    {
+      tip: '... had sex on the beach',
+      rating:  2
+    },
+    {
+      tip: '... had sex on a roof',
+      rating:  2
+    },
+    {
+      tip: '... had sex in a car/bus/boat/bike',
+      rating:  2
+    },
+    {
+      tip: '... had sex at someone\'s work',
+      rating:  3
+    },
+    {
+      tip: '... used a sex toy',
+      rating:  3
+    },
+    {
+      tip: '... used an item to masturbate',
+      rating:  3
+    },
+    {
+      tip: '... travelled overseas',
+      rating:  1
+    },
+    {
+      tip: '... broken a bone',
+      rating:  1
+    },
+    {
+      tip: '... gone skinny dipping',
+      rating:  1
+    },
+    {
+      tip: '... done a nudie run',
+      rating:  1
+    },
+    {
+      tip: '… used 3 fingers sexually',
+      rating:  3
+    },
+    {
+      tip: '… had sex in the same room as someone else',
+      rating:  2
+    },
+    {
+      tip: '… waxed',
+      rating:  1
+    },
+    {
+      tip: '… got a tattoo',
+      rating:  1
+    },
+    {
+      tip: '… slept with 2 people in one day/week',
+      rating:  2
+    },
+    {
+      tip: '… masturbated and have sex in the same day',
+      rating:  3
+    },
+    {
+      tip: '… stole from my parents',
+      rating:  1
+    },
+    {
+      tip: '… stole from a shop',
+      rating:  1
+    },
+    {
+      tip: '… smoked a cigar/ciggy/spliff',
+      rating:  1
+    },
+    {
+      tip: '… cheated on someone',
+      rating:  2
+    }
+  ],
+  categories:[
+    {
+      tip: 'Olympic events',
+      rating:  1
+    },
+    {
+      tip: 'Alcohol brands',
+      rating:  1
+    },
+    {
+      tip: 'Types of trees',
+      rating:  1
+    },
+    {
+      tip: 'Beer brands',
+      rating:  1
+    },
+    {
+      tip: 'house pets',
+      rating:  1
+    },
+    {
+      tip: 'sex toys',
+      rating:  3
+    },
+    {
+      tip: 'sex positions',
+      rating:  2
+    },
+    {
+      tip: 'countries',
+      rating:  1
+    },
+    {
+      tip: 'Car brands',
+      rating:  1
+    },
+    {
+      tip: 'Porn categories',
+      rating:  3
+    },
+    {
+      tip: 'Countries',
+      rating:  1
+    },
+    {
+      tip: 'Shoe brands',
+      rating:  1
+    },
+    {
+      tip: 'Consoles brand',
+      rating:  1
+    },
+    {
+      tip: 'PC brands',
+      rating:  1
+    },
+    {
+      tip: 'Condom brands',
+      rating:  2
+    },
+    {
+      tip: 'Tampon brands',
+      rating:  2
+    },
+    {
+      tip: 'Fetishes',
+      rating:  3
+    },
+    {
+      tip: 'Games you played at school',
+      rating:  1
+    },
+    {
+      tip: 'PC games you played at school',
+      rating:  1
+    },
+    {
+      tip: 'Things you can only put on a pizza',
+      rating:  1
+    }
+  ],
+  makeARule:[
+    {
+      tip: 'Little green man: take an invisible figure off your glass when you drink and place it back when you finished',
+      rating:  1
+    },
+    {
+      tip: 'Name switch: Switch names to the left for the rest of the game',
+      rating:  1
+    },
+    {
+      tip: 'Gentleman\'s Rules: No swearing + no pointing + no first names',
+      rating:  2
+    },
+    {
+      tip: 'Double Tap: When you place your drink down you must tap it twice',
+      rating:  1
+    },
+    {
+      tip: 'Every 5 seconds you take to draw your card you drink',
+      rating:  2
+    },
+    {
+      tip: 'Complain train (No complaining)',
+      rating:  1
+    },
+    {
+      tip: 'Before picking a card guess the colour. Correct; give a drink. Wrong; take a drink',
+      rating:  1
+    },
+    {
+      tip: 'Floor is lava: no touching the ground',
+      rating:  1
+    },
+    {
+      tip: 'Acid skin: your skin can\'t touch',
+      rating:  1
+    },
+    {
+      tip: 'Pirate life: speak like a pirate',
+      rating:  1
+    },
+    {
+      tip: 'In my pants: say in my pants at the end of every sentence',
+      rating:  2
+    },
+    {
+      tip: 'Snake eyes: anyone look in each others eyes both drink',
+      rating:  1
+    },
+    {
+      tip: 'Every black card remove an item of clothing (pairs=1)',
+      rating:  3
+    },
+    {
+      tip: 'Odd cards boys drink Even girls',
+      rating:  2
+    },
+    {
+      tip: 'End every sentence with; if you know what I mean',
+      rating:  1
+    },
+    {
+      tip: 'Swearing = -1 item of clothing',
+      rating:  3
+    },
+    {
+      tip: 'Foreign accents only',
+      rating:  1
+    },
+    {
+      tip: 'T-rex: Everyone lock their elbows to their sides for the rest of the game',
+      rating:  1
+    },
+    {
+      tip: 'Pajama party: everyone in your undies',
+      rating:  3
+    },
+    {
+      tip: 'Add ‘if you know what i mean’ to the end of every sentence',
+      rating:  1
+    },
+    {
+      tip: 'Runway; if you get a spades run around the group pretending to be a plain',
+      rating:  1
+    },
+    {
+      tip: 'Mr Freeze; the moment you stop moving. everyone else has to. First person to move loose',
+      rating:  1
+    },
+    {
+      tip: 'Say ‘in my vagina’ after every drink',
+      rating:  2
+    },
+    {
+      tip: 'Rollover DJ: pass the music device around. Each player puts a song on (throughout the game). If majority of the group approve give a drink else take one. Forget to … Skull',
+      rating:  1
+    },
+    {
+      tip: 'If the group knows the game mafia. As soon as King Queen Jack and necessary plebs. Play mini game of Mafia',
+      rating:  1
+    }
+  ],
+  questionMaster:[
+    {
+      tip: 'What does this card do again?',
+      rating:  1
+    },
+    {
+      tip: 'What time is it?',
+      rating:  1
+    },
+    {
+      tip: 'Whos turn is it?',
+      rating:  1
+    },
+    {
+      tip: 'Why can\'t you do that?',
+      rating:  1
+    },
+    {
+      tip: 'Where\'s the bathroom?',
+      rating:  1
+    },
+    {
+      tip: 'Does anyone want a drink?',
+      rating:  1
+    },
+    {
+      tip: 'Is this game finished?',
+      rating:  1
+    },
+    {
+      tip: 'What’s your last name?',
+      rating:  1
+    },
+    {
+      tip: 'When’s your birthday?',
+      rating:  1
+    },
+    {
+      tip: 'Are you single?',
+      rating:  1
+    },
+    {
+      tip: 'Want to bang?',
+      rating:  2
+    },
+    {
+      tip: 'Anal?',
+      rating:  3
+    },
+    {
+      tip: 'Do you like BDSM?',
+      rating:  2
+    },
+    {
+      tip: 'League of legends?',
+      rating:  1
+    }
+  ],
+  mostLikelyToo:[
+    {
+      tip: '... pass out',
+      rating:  1
+    },
+    {
+      tip: '... throw up',
+      rating:  1
+    },
+    {
+      tip: '... pick up tonight',
+      rating:  2
+    },
+    {
+      tip: '... have a threesome next',
+      rating:  2
+    },
+    {
+      tip: '... flash or do a nudie run',
+      rating:  2
+    },
+    {
+      tip: '... become rich',
+      rating:  1
+    },
+    {
+      tip: '... become famous',
+      rating:  1
+    },
+    {
+      tip: '… be kicked out of a club',
+      rating:  2
+    },
+    {
+      tip: '… be a sexual predator',
+      rating:  3
+    },
+    {
+      tip: '… be in a fight',
+      rating:  2
+    },
+    {
+      tip: '… cheat on someone',
+      rating:  3
+    },
+    {
+      tip: '… hide a dark secret',
+      rating:  2
+    },
+    {
+      tip: '… be the best at sex',
+      rating:  3
+    },
+    {
+      tip: '… give the best head',
+      rating:  3
+    },
+    {
+      tip: '… get on TV',
+      rating:  1
+    },
+    {
+      tip: '… become president',
+      rating:  1
+    },
+    {
+      tip: '... get a nipple piercing',
+      rating:  2
+    },
+    {
+      tip: '… wear underwear as a hat',
+      rating:  1
+    },
+    {
+      tip: '… get a tattoo tonight',
+      rating:  1
+    },
+    {
+      tip: '… get a tattoo of a “<3 mom”',
+      rating:  1
+    },
+    {
+      tip: '… smuggle drugs through an airport',
+      rating: 1
+    },
+    {
+      tip: '… lie about their age',
+      rating:  1
+    },
+    {
+      tip: '… lie about their job',
+      rating:  1
+    },
+    {
+      tip: '... have a child',
+      rating:  2
+    },
+    {
+      tip: '… cross dress for fun',
+      rating:  2
+    },
+    {
+      tip: '… shave their head',
+      rating:  1
+    }
+  ],
+  truth:[
+    {
+      tip: 'Last time you masturbated',
+      rating:  3
+    },
+    {
+      tip: 'Last time you had sex',
+      rating:  2
+    },
+    {
+      tip: 'Have you had sex thinking about someone else',
+      rating:  2
+    },
+    {
+      tip: 'Skinny dipped',
+      rating:  1
+    },
+    {
+      tip: 'Sex in public',
+      rating:  2
+    },
+    {
+      tip: 'Sexted',
+      rating:  2
+    },
+    {
+      tip: 'Do you feel successful in life',
+      rating:  3
+    },
+    {
+      tip: 'Last time you cried',
+      rating:  1
+    },
+    {
+      tip: 'Thought about someone in this room sexually',
+      rating:  3
+    },
+    {
+      tip: 'Had sex in the same room as someone else',
+      rating:  2
+    },
+    {
+      tip: 'Masturbated in the same room as someone else',
+      rating:  3
+    },
+    {
+      tip: 'How many people have you had sex with',
+      rating:  3
+    },
+    {
+      tip: 'Have you ever used sexual toys',
+      rating:  2
+    },
+    {
+      tip: 'Bought sex toys',
+      rating:  2
+    },
+    {
+      tip: 'Your craziest sexual experience',
+      rating:  3
+    },
+    {
+      tip: 'Have you paid for sex',
+      rating:  2
+    },
+    {
+      tip: 'Break in and enter',
+      rating:  2
+    },
+    {
+      tip: 'Your biggest lie',
+      rating:  3
+    },
+    {
+      tip: 'Your biggest regret',
+      rating:  3
+    },
+    {
+      tip: 'Your most embarrassing moment',
+      rating:  2
+    },
+    {
+      tip: 'Your most shameful moment',
+      rating:  2
+    }
+  ],
+  dare:[
+    {
+      tip: 'Eat cereal out someone\'s mouth',
+      rating:  3
+    },
+    {
+      tip: 'Bird feed your drink (Someone gives someone drink from mouth)',
+      rating:  2
+    },
+    {
+      tip: 'Nudie run around the premises whilst finishing your drink',
+      rating:  3
+    },
+    {
+      tip: 'Nude swim in nearest water hole',
+      rating:  3
+    },
+    {
+      tip: 'Rest of the game in your undies',
+      rating:  2
+    },
+    {
+      tip: 'Finish your drink while humming the national anthem',
+      rating:  1
+    }
+  ],
+  Potion; every else concoct a drink or meal for you to eat, 2[
+    {
+      tip: 'Cup of milk and alcohol',
+      rating:  2
+    },
+    {
+      tip: 'Everyone can draw on you',
+      rating:  2
+    },
+    {
+      tip: 'Lick the bottom of someone\'s foot',
+      rating:  1
+    },
+    {
+      tip: 'Kiss 2 peoples armpit',
+      rating:  2
+    },
+    {
+      tip: 'Lick someone chin to forehead',
+      rating:  2
+    },
+    {
+      tip: 'Keep your shoes on your hand for the rest of the game',
+      rating:  2
+    },
+    {
+      tip: 'Do a shoey',
+      rating:  2
+    }
+  ],
+  wouldYouRather:[
+    {
+      tip: 'Eat a dick & sit on a cake OR sit on a dick & eat a cake',
+      rating:  3
+    },
+    {
+      tip: 'Fly OR teleport',
+      rating:  1
+    },
+    {
+      tip: 'Know all languages OR know all musical instruments',
+      rating:  1
+    },
+    {
+      tip: 'Be high OR drunk for a week straight',
+      rating:  1
+    },
+    {
+      tip: 'World famous surfer OR skateboarder',
+      rating:  1
+    },
+    {
+      tip: 'Bungee jump OR skydive',
+      rating:  1
+    },
+    {
+      tip: 'Control minds OR control time',
+      rating:  1
+    },
+    {
+      tip: 'Anorexia or overweight',
+      rating:  2
+    },
+    {
+      tip: 'Tits OR ass',
+      rating:  2
+    },
+    {
+      tip: 'Cowgirl OR doggie',
+      rating:  3
+    },
+    {
+      tip: 'Shaved OR natural',
+      rating:  2}
+    ]
+  };
